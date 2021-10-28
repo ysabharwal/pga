9adf871d0de3f20ec7a0e512492c62935de0ee35
satwik2708 <81012408+satwik2708@users.noreply.github.com>
2021-10-29 00:04:43 +0530

    Delete SSSP_dist_memory directory

--- SSSP_dist_memory/compile.sh
+++
@@ -1,8 +0,0 @@
-if [[ $1 = seq ]]
-then
-	gcc src/dijkstra_seq.c -w -o0 -o bin/dijkstra_seq
-elif [[ $1 = omp ]]
-then
-	gcc src/dijkstra_omp.c -w -o0 -o bin/dijkstra_omp -fopenmp
-fi
-
--- SSSP_dist_memory/run.sh
+++
@@ -1,7 +0,0 @@
-if [[ $1 = seq ]]
-then
-	./bin/dijkstra_seq $2 $3
-elif [[ $1 = omp ]]
-then
-	./bin/dijkstra_omp $2 $3 $4
-fi
